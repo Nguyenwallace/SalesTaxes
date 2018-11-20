04b50f0897a0a8cc7ec738cd2be93029f4b663af
nguyenwallace <nguyenwallace@gmail.com>
2018-11-20 16:15:38 +0700

    seperate SalesTaxes.rb into many class files and put in folder SalesTaxes

--- SalesTaxes/bill.rb
+++ SalesTaxes/bill.rb
@@ -1,5 +1,5 @@
 #class Bill to add and print the Bill
-require './ProductLine'
+require './productLine'
 class Bill
 	attr_accessor :items
 
@@ -33,20 +33,3 @@ class Bill
 	end
 end
 
-bill1 = Bill.new
-bill1.addItem(ProductLine.generate("book", "book", 1, 12.49, "no"))
-bill1.addItem(ProductLine.generate("others", "music cd", 1, 14.99, "no"))
-bill1.addItem(ProductLine.generate("food", "chocolate bar", 1, 0.85, "no"))
-bill1.printBill
-
-bill2 = Bill.new
-bill2.addItem(ProductLine.generate("food", " imported box of chocolates ", 1, 10.00, "import"))
-bill2.addItem(ProductLine.generate("others", "imported bottle of perfume ", 1, 47.50, "import"))
-bill2.printBill
-
-bill3 = Bill.new
-bill3.addItem(ProductLine.generate("others", "imported bottle of perfume", 1, 27.99, "import"))
-bill3.addItem(ProductLine.generate("others", "bottle of perfume", 1, 18.99, "no"))
-bill3.addItem(ProductLine.generate("medical", "packet of headache pills ", 1, 9.75, "no"))
-bill3.addItem(ProductLine.generate("food", "box of imported chocolates ", 1, 11.25, "import"))
-bill3.printBill
